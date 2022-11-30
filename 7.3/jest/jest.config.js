3ca2b67dcafaa1e7b078667f9a5cb8ece6c4c9f5
Амир Байтулинов <amiricus@gmail.com>
2022-11-30 11:59:38 +0500

    First commit

---
+++ 7.3/jest/jest.config.js
@@ -0,0 +1,15 @@
+const config = {
+    collectCoverageFrom: [
+      '**/*.{js,jsx}',
+      '!**/node_modules/**',
+      '!**/coverage/**',
+    ],
+    coverageThreshold: {
+        "branches": 100,
+        "functions": 100,
+        "lines": 100
+  }
+  };
+  
+  module.exports = config;
+
