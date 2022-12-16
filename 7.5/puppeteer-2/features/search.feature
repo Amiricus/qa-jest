39a005bbd830dc7711481d8c13f808dffa3834e8
Амир Байтулинов <amiricus@gmail.com>
2022-12-16 17:12:30 +0500

    Add tests

--- 7.5/puppeteer-2/features/search.feature
+++ 7.5/puppeteer-2/features/search.feature
@@ -1,5 +1,28 @@
-Feature: Search a course
-    Scenario: Should search by text
-        Given user is on "/navigation" page
-        When user search by "тестировщик"
-        Then user sees the course suggested "Тестировщик ПО"
+Feature: Go to the cinema tests
+    Scenario: The first positive test
+        Given user is on start page1
+        When user click element a1
+        And click element b1
+        And click element c1
+        And click element d1
+        Then user sees ver.1 "Получить код бронирования"
+
+    Scenario: The second positive test
+        Given user is on start page2
+        When user click element a2
+        And click element b2
+        And click element c2
+        And click element d2
+        And click element e2
+        Then user sees ver.2 "Получить код бронирования"
+
+    Scenario: The negative test
+        Given user is on start page3
+        When user click element a3
+        And click element b3
+        And click element c3
+        And click element d3
+        Then user sees ver.3 "Забронировать"
+
+
+
--- 7.5/puppeteer-2/features/step_definitions/search.steps.js
+++ 7.5/puppeteer-2/features/step_definitions/search.steps.js
@@ -2,10 +2,10 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After } = require("cucumber");
-const { putText, getText } = require("../../lib/commands.js");
+const { clickElement, getText } = require("../../lib/commands.js");
 
 Before(async function () {
-  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   this.browser = browser;
   this.page = page;
@@ -17,18 +17,74 @@ After(async function () {
   }
 });
 
-Given("user is on {string} page", async function (string) {
-  return await this.page.goto(`https://netology.ru${string}`, {
-    setTimeout: 20000,
+Given("user is on start page1", {timeout: 10000}, async function () {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`);
   });
+
+When("user click element a1", async function () {
+  return await clickElement(this.page, "a.page-nav__day.page-nav__day_today.page-nav__day_chosen");
+});
+When("click element b1", async function () {  
+  return await clickElement(this.page, "section:nth-child(1) > div:nth-child(2) > ul > li > a");
+});
+When("click element c1", async function () {  
+  return await clickElement(this.page, "section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(3) > span:nth-child(1)");
+});
+When("click element d1", async function () {
+  await clickElement(this.page, "button");
 });
 
-When("user search by {string}", async function (string) {
-  return await putText(this.page, "input", string);
+Then("user sees ver.1 {string}", async function (string) {
+  const actual = await getText(this.page, "body > main > section > div > button");
+  const expected = await string;
+  expect(actual).contains(expected);
 });
 
-Then("user sees the course suggested {string}", async function (string) {
-  const actual = await getText(this.page, "a[data-name]");
+Given("user is on start page2", {timeout: 10000}, async function () {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`);
+  });
+
+When("user click element a2", async function () {
+  return await clickElement(this.page, "a:nth-child(2)");
+});
+When("click element b2", async function () {  
+  return await clickElement(this.page, "section:nth-child(2) > div:nth-child(2) > ul > li > a");
+});
+When("click element c2", async function () {  
+  return await clickElement(this.page, "section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(1)");
+});
+When("click element d2", async function () {  
+  return await clickElement(this.page, "section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span.buying-scheme__chair.buying-scheme__chair_vip");
+});
+When("click element e2", async function () {
+  await clickElement(this.page, "button");
+});
+
+Then("user sees ver.2 {string}", async function (string) {
+  const actual = await getText(this.page, "body > main > section > div > button");
+  const expected = await string;
+  expect(actual).contains(expected);
+});
+
+Given("user is on start page3", {timeout: 10000}, async function () {
+  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`);
+  });
+
+When("user click element a3", async function () {
+  return await clickElement(this.page, "a:nth-child(2)");
+});
+When("click element b3", async function () {  
+  return await clickElement(this.page, "section:nth-child(1) > div:nth-child(3) > ul > li > a");
+});
+When("click element c3", async function () {  
+  return await clickElement(this.page, "section > div.buying-scheme > div.buying-scheme__wrapper > div:nth-child(1) > span:nth-child(1)");
+});
+When("click element d3", async function () {
+  await clickElement(this.page, "button");
+});
+
+Then("user sees ver.3 {string}", async function (string) {
+  const actual = await getText(this.page, "button");
   const expected = await string;
   expect(actual).contains(expected);
 });
